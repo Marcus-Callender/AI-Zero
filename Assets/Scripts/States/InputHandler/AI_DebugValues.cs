58d73d26f9b829f838782c394d44c7fc0c426727
Marcus Callender <m.shute@outlook.com>
2017-03-24 15:03:24 +0000

    Added details on the currently active AI status flags

--- Assets/Scripts/States/InputHandler/AI_DebugValues.cs
+++ Assets/Scripts/States/InputHandler/AI_DebugValues.cs
@@ -21,7 +21,9 @@ public class AI_DebugValues : MonoBehaviour
 	private static GUIStyle m_style = null;
 
 	private string m_text;
-	
+
+	private bool[] m_currentInputs = new bool[(int) eAI_InputArray.TOTAL_INPUTS];
+
 	public void Initialize()
 	{
 		m_myIndex = m_index;
@@ -37,9 +39,11 @@ public class AI_DebugValues : MonoBehaviour
 		// resets the index count for the next game
 		m_index--;
 	}
-	
+
 	public void Cycle(eAI_Actions act, int actFlag)
 	{
+		GetFlags(actFlag);
+
 		m_text = "";
 
 		m_text += "Action: ";
@@ -49,6 +53,14 @@ public class AI_DebugValues : MonoBehaviour
 		m_text += "\nFlags: ";
 
 		m_text += actFlag;
+
+		for (int z = 0; z < (int) eAI_InputArray.TOTAL_INPUTS; z++)
+		{
+			if (m_currentInputs[z])
+			{
+				m_text += ("\n" + (eAI_InputArray) z);
+			}
+		}
 	}
 
 	void OnGUI()
@@ -71,5 +83,36 @@ public class AI_DebugValues : MonoBehaviour
 			m_style = new GUIStyle();
 			m_style.fontSize = 28;
 		}
+	}
+
+	void GetFlags(int flag)
+	{
+		int flagCount = 0;
+
+		for (int z = 0; z < (int) eAI_InputArray.TOTAL_INPUTS; z++)
+		{
+			m_currentInputs[z] = false;
+		}
+
+		for (int z = ((int) eAI_InputArray.TOTAL_INPUTS - 1); z >= 0 ; z--)
+		{
+			if (flagCount + IntToFlag(z) <= flag)
+			{
+				flagCount += IntToFlag(z);
+				m_currentInputs[z] = true;
+			}
+		}
+	}
+
+	int IntToFlag(int z)
+	{
+		int flag = 1;
+
+		for (int x = 1; x < z; x++)
+		{
+			flag *= 2;
+		}
+
+		return flag;
 	}
 }
